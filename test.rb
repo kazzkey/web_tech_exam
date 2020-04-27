f6d57f261baadde34448f0b70a353b7b14883c1c
Kazuki Kawano <k_kawano86@yahoo.co.jp>
2020-04-27 17:23:03 +0900

    web_tech_examを作成

---
+++ test.rb
@@ -0,0 +1,13 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'goyainfo.html.erb')
+server.mount('/goya1.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya1.rb')
+server.mount('/goya2.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya2.rb')
+server.start
